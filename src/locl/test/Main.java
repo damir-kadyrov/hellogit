c267f06b7f801fde83a7c85a0024fb5d9879cd54
damir-kadyrov <kadyrovdamiri@gmail.com>
2013-01-03 11:59:11 +0300

    first commit

---
+++ src/locl/test/Main.java
@@ -0,0 +1,11 @@
+package locl.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: damir
+ * Date: 03.01.13
+ * Time: 12:58
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+}
